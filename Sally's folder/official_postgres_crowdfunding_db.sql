a3225befe98d14a7c864aeba293b293622197538
sallyca530 <sally.ca530@gmail.com>
2024-01-04 20:45:49 -0800

    added select from statements

--- Sally's folder/official_postgres_crowdfunding_db.sql
+++ Sally's folder/official_postgres_crowdfunding_db.sql
@@ -9,7 +9,7 @@ DROP TABLE if exists campaign;
 DROP TABLE if exists category;
 DROP TABLE if exists subcategory;
 DROP TABLE if exists contacts;
-
+-----------------------------------------------------------------
 CREATE TABLE "category" (
     "category_id" varchar(50)   NOT NULL,
     "category" varchar(50)   NOT NULL,
@@ -17,7 +17,8 @@ CREATE TABLE "category" (
         "category_id"
      )
 );
-
+Select * From "category";
+-----------------------------------------------------------------
 CREATE TABLE "subcategory" (
     "subcategory_id" varchar(50)   NOT NULL,
     "subcategory" varchar(50)   NOT NULL,
@@ -25,14 +26,15 @@ CREATE TABLE "subcategory" (
         "subcategory_id"
      )
 );
-
+Select * From "subcategory";
+-----------------------------------------------------------------
 CREATE TABLE "campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
     "company_name" varchar(100)   NOT NULL,
     "description" varchar(100)   NOT NULL,
-    "goal" int   NOT NULL,
-    "pledged" int   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
     "outcome" varchar(50)   NOT NULL,
     "backers_count" int   NOT NULL,
     "country" varchar(50)   NOT NULL,
@@ -45,7 +47,8 @@ CREATE TABLE "campaign" (
         "contact_id"
      )
 );
-
+Select * From "campaign";
+-----------------------------------------------------------------
 CREATE TABLE "contacts" (
     "contact_id" int   NOT NULL,
     "first_name" varchar(50)   NOT NULL,
@@ -55,7 +58,8 @@ CREATE TABLE "contacts" (
         "contact_id"
      )
 );
-
+Select * From "contacts";
+-----------------------------------------------------------------
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
 REFERENCES "category" ("category_id");
 
